9deb21e61572da20cc5618aa0aea69fd7a8bc5e1
Dhirendra <dhirendrabhoi40@gmail.com>
2026-05-15 14:54:23 +0530

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import ProcessingSection from './components/ProcessingSection';
 import ResultSection from './components/ResultSection';
 import ErrorBanner from './components/ErrorBanner';
 
-// const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000'; 
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000'; 
 
 
 /**
